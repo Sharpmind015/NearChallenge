a3328baa1e3a355b76f14c1bc3d4d90a57629e2d
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-20 21:32:15 +0100

    remove default behaviour for form submit event

--- custom-hook/useSearch.tsx
+++ custom-hook/useSearch.tsx
@@ -8,7 +8,7 @@ const useSearch = () => {
     setSearch(e.target.value);
   }
   const Search : FC = () =>  (
-    <form>
+    <form onSubmit={(e) => e.preventDefault()}>
       <label className='relative' htmlFor='Search'>
         <Image className='absolute left-3' src="/search.svg" width='20' height='20' alt="" />
         <input value='search' onChange={onChange} className='px-8 py-3 text-gray-500 text-base bg-gray-700 rounded-lg' type="search" placeholder='Search experiences' />
