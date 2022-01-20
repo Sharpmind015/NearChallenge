2ed4b664d918b7ff3288e2defff0987e2c5afbc5
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-20 22:05:28 +0100

    feat : build search form

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -3,8 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Container from '../layouts/Container'
 import Topbar from '../layouts/Topbar'
+import {useState} from 'react'
 
 const Experience: NextPage = () => {
+  const [search, setSearch] = useState<string>('');
+  const onChange : (e : React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+    setSearch(e.target.value);
+  }
   return (
     <div className="">
       <Head>
@@ -15,6 +20,19 @@ const Experience: NextPage = () => {
       <Container>
         <main className="">
           <Topbar />
+          <div className='mt-3 px-6 flex justify-between'>         
+            <form onSubmit={(e) => e.preventDefault()}>
+              <label className='relative w-269px w-1/2 h-12' htmlFor='Search'>
+                <svg className='absolute left-3 top-1/2 -translate-y-1/2' width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M18.481 17.5L14.7426 13.755L18.481 17.5ZM16.8143 8.74999C16.8143 10.6286 16.068 12.4303 14.7397 13.7587C13.4113 15.087 11.6096 15.8333 9.73098 15.8333C7.85236 15.8333 6.05069 15.087 4.7223 13.7587C3.39392 12.4303 2.64764 10.6286 2.64764 8.74999C2.64764 6.87137 3.39392 5.0697 4.7223 3.74132C6.05069 2.41293 7.85236 1.66666 9.73098 1.66666C11.6096 1.66666 13.4113 2.41293 14.7397 3.74132C16.068 5.0697 16.8143 6.87137 16.8143 8.74999V8.74999Z" stroke="#6F6E73" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+                <input value={search} onChange={onChange} className='px-12 py-3 text-gray-500 text-base bg-gray-700 rounded-lg  focus:ring-2 focus:ring-accent-200 focus:ring-opacity-75 outline-none w-269px' type="text" placeholder='Search experiences' />
+              </label>
+            </form>
+            <button>
+              <Image src="/filter.svg" width='24' height='24' alt='' />
+            </button>
+          </div>
         </main>
       </Container>
     </div>
