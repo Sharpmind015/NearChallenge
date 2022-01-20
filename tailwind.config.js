903ca74e5b20090c9fa4b88abdd7cfd8bb91166f
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-20 11:22:58 +0100

    feat : configure tailwind colors to match design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'blue': {
+        100: '#587BE0',
+        200: '#799CFF',
+        300: '#9DB7FF',
+        400: '#CFDCFF',
+        500: '#EAEFFF'
+      },
+      'accent': {
+        100: '#B072FF',
+        200: '#885FFF',
+        300: '#3BD0AC',
+      },
+      'gray': {
+        100: '#33373B',
+        200: '#414047',
+        300: '#6F6E73',
+        400: '#A0A0A1',
+        500: '#BEBEC2',
+        600: '#DFDFE0'
+      },
+    },
     extend: {},
     fontFamily: {
       sans: ['Manrope', 'sans-serif'],
