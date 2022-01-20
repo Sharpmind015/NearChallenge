482c0fdb8481d851f5e9a67e7eb13578463e9927
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-20 10:07:37 +0100

    feat : build the mobile container layout

---
+++ layouts/Container.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react';
+
+interface Props {
+  children: JSX.Element
+}
+
+const Container : FC<Props> = ({children}) => {
+  return (
+    <div className="mobile-container">
+      {children}
+    </div>
+  )
+}
+
+export default Container;
