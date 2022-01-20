933293d3676ab73288efb337244609115d24ff9f
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-20 11:11:48 +0100

    feat : build topbar interface for topbar

---
+++ layouts/AuthTopbar.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+
+interface Props {
+  type: string,
+  title?: string
+
+}
+
+const AuthTopbar : FC<Props> = ({type, title}) => {
+  return (
+    <div className="w-full">
+    </div>
+  )
+} 
+
+export default AuthTopbar;
