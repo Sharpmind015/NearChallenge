7bb3a4093b0d27757dfc852df4fe990d85afed16
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-21 13:34:39 +0100

    feat : add history navigation to back button

--- layouts/Topbar.tsx
+++ layouts/Topbar.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router'
 import {FC} from 'react';
 
 
 const Topbar : FC = () => {
+  const router = useRouter()
   return (
     <header className="flex justify-between items-center bg-gray-700 py-3 px-6 flex-wrap h-20 fixed top-0 left-0 w-full z-50">
-      <Image src="/arrow.svg" width="20" height="22" alt="" />
+      <button type='button' className='flex justify-center items-center' onClick={() => router.back()}>
+        <Image src="/arrow.svg" width="20" height="22" alt="" />
+      </button>
       <Image src="/bookmark.svg" width="20" height="22" alt="" />
       <div className='pl-3 pr-4 flex justify-between items-center py-3 bg-white rounded-30px' >
         <Image src="/profile.svg" width="26" height="26" alt="" />
