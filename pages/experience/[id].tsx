2864391ca9f5f45c33ce8d2b6ce6799ba8391234
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-21 01:21:53 +0100

    feat : build experience page

--- pages/experience/[id].tsx
+++ pages/experience/[id].tsx
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Tab } from '@headlessui/react'
 import Container from '../../layouts/Container'
 import Topbar from '../../layouts/Topbar'
 
@@ -17,7 +18,40 @@ const Experience: NextPage = () => {
       <Container>
           <div>
           <Topbar />
+          <div style={{backgroundColor: '#E5D0FF'}} className='h-14 mt-20'></div>
           <main className="px-6">
+            <div className='flex justify-between'>
+              <div className='-mt-7'>
+                <Image src='/Docu.png' width='69' height='69' alt='' />
+              </div>
+              <div className='flex items-center'>
+                <Image src='/share.svg' width='18' height='18' alt='' />
+                <span className='text-12px font-medium leading-5 not-italic text-blue-100 ml-2'>Share</span>
+              </div>
+            </div>
+            <h2 className='heading heading--2 mt-3'>Docu sign</h2>
+            <p className='text-gray-300 text-sm leading-4 not-italic mt-1'>sign smart contracts seamlessly</p>
+            <p className='text-sm font-medium leading-5.5 not-italic text-blue-100 mt-2'>Utilities</p>
+            <div className='flex items-center mt-4'>
+              <button className='btn btn-gray-200 flex mr-3'>
+                <span className='mr-4'>Open</span>
+                <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16.9849 9.414L8.37793 18.021L6.96393 16.607L15.5699 8H7.98493V6H18.9849V17H16.9849V9.414Z" fill="white"/>
+                </svg>
+              </button>
+              <Image src='/users.svg' width='16' height='16' alt='' />
+              <p className='text-gray-300 text-sm leading-5.5 not-italic ml-2'>24,000+ users</p>
+            </div>
+            <Tab.Group>
+              <Tab.List>
+                <Tab>Tab 1</Tab>
+                <Tab>Tab 2</Tab>
+              </Tab.List>
+              <Tab.Panels>
+                <Tab.Panel>Content 1</Tab.Panel>
+                <Tab.Panel>Content 2</Tab.Panel>
+              </Tab.Panels>
+            </Tab.Group>
           </main>
         </div>
       </Container>
