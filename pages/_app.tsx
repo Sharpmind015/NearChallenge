014df8fcb122f0b9d30ddff53b0bb224baa7938c
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-21 15:21:58 +0100

    init setup for react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
-import { Provider } from 'react-redux';
-import { store } from '../app/store';
+import { Provider } from 'react-redux'
+import { store } from '../app/store'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
   )
 }
 
